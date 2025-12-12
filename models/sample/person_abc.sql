d3ef87e67b25f2b31eb17f62ad366a44ebbb3086
Thiago Nicino Menezes <assis.thiago@gmail.com>
2025-12-12 20:06:31 +0000

    my_changes

--- models/sample/person_abc.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'John' as first_name, 'Doe' as last_name, '1970-01-01' as birthdate
-union all
-SELECT 'Jane' as first_name, 'Smith' as last_name, '1985-03-15' as birthdate
-union all 
-SELECT 'Bob' as first_name, 'Johnson' as last_name, '1978-11-22' as birthdate
--- models/sample/test.sql
+++
@@ -1,5 +0,0 @@
-select 
-    customer_id, 
-    SUM(order_amount) as total_revenue
-from public.transactions
-group by customer_id
